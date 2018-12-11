51a927dcfe427515b8b1500c40548dbe0a000351
Kristen <29871623+pikap3w@users.noreply.github.com>
2018-12-11 15:06:29 -0600

    like

--- kristen_martino/ruby_stack/5_Rails_II/DojoSecrets/spec/models/like_spec.rb
+++ kristen_martino/ruby_stack/5_Rails_II/DojoSecrets/spec/models/like_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    before do
+      @user   = create(:user)
+      @secret = create(:secret, user: @user)
+      @like   = create(:like, secret: @secret, user: @user)
+    end
+    it 'belongs to a user' do
+      expect(@like.user).to eq(@user)
+    end
+    it 'belongs to a secret' do
+      expect(@like.secret).to eq(@secret)
+    end
+  end
 end
+https://github.com/pikap3w/ruby_dec_2018/tree/dec10/kristen_martino/ruby_stack/5_Rails_II/DojoSecrets 
